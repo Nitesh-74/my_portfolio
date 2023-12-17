645f6b7e854e55ea9a8d776596f269b82a87a4b2
Nitesh-74 <2020bit001@sggs.ac.in>
2023-12-17 16:45:55 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,231 @@
+import tailwindImage from "../images/tailwindImage.jpeg"
+import oopimage from "../images/oop.jpg"
+import dsimage from "../images/ds.jpg"
+import cppimage from "../images/cpp.png"
+import flex from "../images/flex.png"
+import gen from "../images/passgen.png"
+import solver from "../images/Sudoku-Solved.png"
+import clone from "../images/clone.png"
+import forecast from "../images/weather.png"
+import sggs from "../images/sggs.jpeg"
+import school from "../images/school.png"
+
+export const Bio = {
+    name: "NITESH SINGH",
+    roles: [
+      "Front-end Developer",
+      "Compettive Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Nitesh-74",
+    resume:
+      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/niteshsingh74/",
+   
+    insta: "https://www.instagram.com/nitesh__singh____/",
+
+    codechef: "https://www.codechef.com/users/nitesh_74",
+    leetcode:"https://leetcode.com/Nitesh_Singh_7/",
+    geeksforgeeks:"https://auth.geeksforgeeks.org/user/niteshsingh74"
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+      
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Tailwind",
+          image:tailwindImage, innerWidth:20,
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Programming",
+      skills: [
+        {
+          name: "C/C++",
+          image:cppimage,
+        },
+        {
+          name: "JavaScript",
+          image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "OOP",
+          image: oopimage,
+        },
+        {
+          name: "Data Structure",
+          image: dsimage
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+      
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+       
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: sggs,
+      school: "Shri Guru Gobind Singh Ji Institute Of Engineering and Technology,vishnupuri,Nanded",
+      date: " 2020 - 2024",
+      grade: "8.00 CGPA",
+      desc: "I am currently pursuing a Bachelor's degree in Information Technology at Shri Guru Gobind Singh Ji Institute Of Engineering and Technology,vishnupuri,Nanded. I have completed 6 semesters and have a CGPA of 8.0. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+      degree: "Bachelor of Technology - BTech, Information Technology",
+    },
+    {
+      id: 1,
+      img:school,
+      school: "Government Higher Secondary School ,Nagrota",
+      date: "Apr 2018 - Feb 2019",
+      grade: "79.2%",
+      desc: "I completed my class 12 high school education atGovernment Higher Secondary School ,Nagrota, where I studied Math with Information Practices.",
+      degree: "Higher Secondary Education",
+    },
+    {
+      id: 2,
+      img:school,
+      school: " Shanti Niketan Education Institute ,Nagrota",
+      date: "May 2016 - Mar 2017",
+      grade: "81.9%",
+      desc: "I completed my class 10 education at Shanti Niketan Education Institute ,Nagrota, where I studied Math and Science.",
+      degree: "Secondary School Education",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "ZENFLEX",
+      image:flex,
+      description:
+        "ZENFLEx is a web application designed to provide information regarding the gyms ethic values and their work",
+         
+      tags: [
+        "HTMLr",
+        "CSS",
+        "JS",
+       "VSCODE"
+      ],
+      category: "web app",
+      github: "https://github.com/Nitesh-74/fitness-website",
+    },
+    {
+      id: 0,
+      title: "Password Generator",
+      image:gen,
+      description:
+        "Developed a  web application that allows users to search for random strong passwords.It also calculates the strength of your passwod",
+      tags: ["HTML", "CSS", "JK", "TAILWIND"],
+      category: "web app",
+      github: "https://github.com/Nitesh-74/password-generator",
+    },
+    {
+      id: 1,
+      title: "SUDUKU SOLVER",
+      image:solver,
+      description:
+      "It a puzzle game implemented using a backtracking algorithm",
+      tags: [
+        "Js",
+        "DSA",
+        "Backtackin",
+        "CSS",
+      ],
+      category: "web app",
+      github: "https://github.com/Nitesh-74/Sudoku_Solver", 
+    },
+    {
+      id: 2,
+      title: "Google Translate Clone",
+      image:clone,
+      description:
+        "It a clone of the google translate that converts the text into any other language you want to translate",
+      
+      tags: ["React jS"],
+      category: "web app",
+      github: "https://github.com/Nitesh-74/translate_app",
+    },
+    {
+      id: 2,
+      title: "Weather Application",
+      image:forecast,
+      description:
+        "It a web application implemented using javascript,css that gives us the imformation about weather forecast",
+      
+      tags: ["javascript,css,API"],
+      category: "web app",
+      github: "https://github.com/Nitesh-74/weather_app",
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
