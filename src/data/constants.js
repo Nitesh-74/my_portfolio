5f8af2e3bef269b853041d3fa72b44ceb617b453
Nitesh-74 <2020bit001@sggs.ac.in>
2023-12-21 01:05:15 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -20,7 +20,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Nitesh-74",
     resume:
-      "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+      "https://drive.google.com/file/d/1i0OH4yfg7DsUIOtaoJ_vSi6z7x-E8sIU/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/niteshsingh74/",
    
     insta: "https://www.instagram.com/nitesh__singh____/",
